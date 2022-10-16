ed47ec9a5903295f93ee4afc1a782cd1a01b9e30
amithasivaji24 <95175527+amithasivaji24@users.noreply.github.com>
2022-10-16 13:52:25 +0530

    Update Driver.java

--- paymoneyapp/src/com/greatlearning/Driver.java
+++ paymoneyapp/src/com/greatlearning/Driver.java
@@ -21,7 +21,7 @@ public class Driver {
 		int noOfTargets = sc.nextInt();
 		Service service = new Service();
 		for(int i = 0; i<noOfTargets; i++) {
-			System.out.println("Enter the value of target");
+			System.out.println("Enter the value of target " + i);
 			int targetValue = sc.nextInt();
 			service.targetAchievement(transactionArray, size, targetValue);
 		}
